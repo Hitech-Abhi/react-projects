72964f16d75e5c0b25eba12db531bceb02564bf6
Abhi <hitechabhi360@gmail.com>
2023-06-22 04:57:20 +0530

    Add todos-list app

---
+++ todos-list/src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, ea
+        veritatis architecto tempore aut libero culpa optio. Aspernatur,
+        perspiciatis aut, earum porro blanditiis dignissimos cum ipsum, aliquid
+        suscipit ducimus molestiae. Amet debitis neque nisi.
+      </p>
+    </div>
+  );
+};
